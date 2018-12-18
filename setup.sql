0eeaf9f9ddea6c5683a5042707eb5322cd698159
Joshua Maza <joshuasadirimaza@gmail.com>
2018-12-17 22:35:17 -0800

    Initial commit

---
+++ setup.sql
@@ -0,0 +1,45 @@
+-- Joshua Maza
+-- 12/17/2018
+-- Rainy Dawg Radio Database System
+-- This file sets up the database for the music database at Rainy Dawg Radio.
+-- The database consists of three tables, intended to be joined for referencing.
+-- Each of the three tables, "Artists", "Song", and "Album" each have an id
+-- column for each entry, along with additional columns depending on purpose.
+-- Ex: The columns for artist and album for "Songs", are intended to correspond
+-- to primary keys (IDs) of the appropriate artist and album.
+
+DROP DATABASE IF EXISTS rdr_song_db;
+CREATE DATABASE rdr_song_db;
+USE rdr_song_db;
+
+-- Artists Table
+DROP TABLE IF EXISTS Artists;
+CREATE TABLE Artists {
+  id int NOT NULL AUTO_INCREMENT,
+  date_created DATETIME NOT NULL DEFAULT(GETDATE())
+  name VARCHAR(100),
+  PRIMARY KEY (id)
+};
+
+-- Song Table
+DROP TABLE IF EXISTS Songs;
+CREATE TABLE Songs {
+  id int NOT NULL AUTO_INCREMENT,
+  date_created DATETIME NOT NULL DEFAULT(GETDATE())
+  name VARCHAR(100),
+  artist INT,
+  album INT,
+  genre VARCHAR(100),
+  PRIMARY KEY (id)
+};
+
+-- Album Table
+DROP TABLE IF EXISTS Album;
+CREATE TABLE Album {
+  id int NOT NULL AUTO_INCREMENT,
+  date_created DATETIME NOT NULL DEFAULT(GETDATE())
+  name VARCHAR(100),
+  artist INT,
+  release_date DATE(),
+  PRIMARY KEY (id)
+};
